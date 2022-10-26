b6fcdca7a5e7a0c97c763a1059c8d4b149e382ef
Ryoidenshi Aokigahara <ryoidenshiaokigahara@gmail.com>
2022-10-26 14:24:53 +0300

    решето Эратосфена сделано

---
+++ procedural-programming/homework-5/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include "euclid.h"
+#include "eratosfen.h"
+using namespace std;
+
+int main(){
+   /*
+  cout << "Задача <Алгоритм Евклида>" << endl;
+  bool euclidDone = false;
+  while(!euclidDone){
+    euclidDone = runEuclid();
+    cout << endl;
+  }
+  cout << endl << endl;
+   */
+
+  // /*
+  cout << "Задача <Решето Эратосфена>" << endl;
+  bool simplesDone = false;
+  while(!simplesDone){
+    simplesDone = findSimples();
+    cout << endl;
+  }
+  cout << endl << endl;
+  // */
+}
